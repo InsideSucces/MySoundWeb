55749b9c93a59c24eb9486a9fbf6d4245b1572b3
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-08-19 17:56:24 +0100

    patching

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,41 +41,5 @@ export const HeroSection = () => {
                 </div>
             </div>
         </section>
-
-
-
-        // <section className="relative min-h-screen flex items-center overflow-hidden">
-        //     <div className="absolute inset-0 bg-gradient-to-br from-[#0f5881] via-[#0a4d74] to-[#2fabf3] z-0"></div>
-        //     <div className="absolute  right-0 h-full w-1/2 z-10">
-        //         <img
-        //             src="/assets/hero_image.png"
-        //             alt="Hero Image"
-        //             className="mx-auto"
-        //         />
-        //     </div>
-        //     <div className="absolute inset-0 flex flex-col justify-center items-center z-10 overflow-hidden">
-        //         <div className="text-[#FEFEFE] text-[570px] font-bold opacity-10 mt-20">MUSIC</div>
-        //         <div className="text-[#FEFEFE] text-[570px] font-bold opacity-10 mt-10">MYSOUND</div>
-        //     </div>
-        //     <div className="container mx-auto z-20 relative">
-        //         <div className="text-left">
-        //             <h1 className="text-9xl lg:text-9xl md:text-4xl font-bold text-white mb-6">
-        //                 Sounds For <br /> Every Moment
-        //             </h1>
-        //             <p className="text-[80px] text-white mb-8 font-bold ">
-        //                 <span className="text-[#61C1C9]">Discover</span> Your Favorite Sounds
-        //             </p>
-        //             <div className="flex flex-row items-center gap-28 px-32 mt-24">
-        //                 <button className="origin-top-left w-40 h-11  bg-gradient-to-tl from-teal-300 to-sky-500 rounded-3xl text-neutral-900 font-bold py-3 px-8 border-emerald-300 blur-none hover:bg-opacity-90 transition duration-300">
-
-        //                     Listen Now
-        //                 </button>
-        //                 <button className="origin-top-left w-40 h-11 bg-transparent text-neutral-900 border-2 border-[#61C1C9] font-bold py-3 px-8 rounded-3xl hover:bg-opacity-90 transition duration-300">
-        //                     Sign Up
-        //                 </button>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
     )
 }
