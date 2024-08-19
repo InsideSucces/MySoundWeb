1a5931702deb293311d42cbaecd5648b61f984a2
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-08-19 13:32:24 +0100

    NavbarMenuToggle

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -51,6 +51,7 @@ export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="full" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarMenuToggle />
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <img
