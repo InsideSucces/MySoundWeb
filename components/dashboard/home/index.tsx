a8f6cbcc15643a120e50f493728e323d4adcc516
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-08-23 18:26:56 +0100

    flexible

--- components/dashboard/home/index.tsx
+++ components/dashboard/home/index.tsx
@@ -67,12 +67,19 @@ const songData = [
 
 export const Home: NextPage = () => {
   return (
-    <div className="h-full px-3 lg:px-6">
-      <div className="">
+    <div className="h-full px-9 lg:px-6">
+      <div className="py-[24.50px]">
+        <div className="flex flex-row justify-between items-center inline-flex w-full ">
+          <div className="text-white text-xl font-medium font-roboto leading-normal tracking-wide">
+            Charts: Top 50
+          </div>
+          {/* ... (Your existing code for the buttons) */}
+        </div>
         <Charts />
       </div>
       <div className="flex flex-col md:flex-row justify-center">
         <div className="md:w-2/3 items-start">
+
           <ListeningHistory songData={songData} />
         </div>
         <div className="md:w-1/3 w-full flex justify-center mt-[90px] md:ml-[190px]">
