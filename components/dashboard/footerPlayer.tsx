4284de40325bf8a8aec3e7bd1e7d00cab1da6412
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2024-09-02 09:40:42 +0100

    remove unnecessary buttons

--- components/dashboard/footerPlayer.tsx
+++ components/dashboard/footerPlayer.tsx
@@ -32,7 +32,7 @@ export const FooterMusicPlayer: FC = () => {
                     </div>
                 </div>
                 <div className="flex items-center justify-center gap-4 md:ml-9 m-9">
-                    <button className="text-zinc-400 hover:text-[#2dcece] transform transition duration-300 ease-in-out hover:scale-125">
+                    {/* <button className="text-zinc-400 hover:text-[#2dcece] transform transition duration-300 ease-in-out hover:scale-125">
                         <svg className="w-4 h-4 text-current fill-current" viewBox="0 0 16 16">
                             <path d="M11.196 8 6 5v6l5.196-3z"></path>
                             <path d="M15.002 1.75A1.75 1.75 0 0 0 13.252 0h-10.5a1.75 1.75 0 0 0-1.75 1.75v12.5c0 .966.783 1.75 1.75 1.75h10.5a1.75 1.75 0 0 0 1.75-1.75V1.75zm-1.75-.25a.25.25 0 0 1 .25.25v12.5a.25.25 0 0 1-.25.25h-10.5a.25.25 0 0 1-.25-.25V1.75a.25.25 0 0 1 .25-.25h10.5z"></path>
@@ -46,7 +46,7 @@ export const FooterMusicPlayer: FC = () => {
                     <button className="text-zinc-400 hover:text-[#2dcece] transform transition duration-300 ease-in-out hover:scale-125">
                         <svg className="w-4 h-4 text-current fill-current" viewBox="0 0 16 16">
                             <path d="M6 2.75C6 1.784 6.784 1 7.75 1h6.5c.966 0 1.75.784 1.75 1.75v10.5A1.75 1.75 0 0 1 14.25 15h-6.5A1.75 1.75 0 0 1 6 13.25V2.75zm1.75-.25a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h6.5a.25.25 0 0 0 .25-.25V2.75a.25.25 0 0 0-.25-.25h-6.5zm-6 0a.25.25 0 0 0-.25.25v6.5c0 .138.112.25.25.25H4V11H1.75A1.75 1.75 0 0 1 0 9.25v-6.5C0 1.784.784 1 1.75 1H4v1.5H1.75zM4 15H2v-1.5h2V15z"></path><path d="M13 10a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm-1-5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path></svg>
-                    </button>
+                    </button> */}
                     <VolumeControl />
                 </div>
             </div>
